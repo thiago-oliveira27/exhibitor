a2f20e80591eca7fbf0b1a59c228e9078cc026a4
thiago <thiagoolcarv27@gmail.com>
2023-05-27 12:56:05 -0300

    Getting Started

---
+++ src/main/java/com/poo/exhibitor/ExhibitorApplication.java
@@ -0,0 +1,13 @@
+package com.poo.exhibitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExhibitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExhibitorApplication.class, args);
+	}
+
+}
